2f5bc818ed80835d745d41e0d14389e0a6236316
nowshin5140 <nowshin5140@gmail.com>
2023-05-08 09:55:06 +0530

    changes have been committed.

--- Typecasting.java
+++ Typecasting.java
@@ -10,14 +10,14 @@ public class Typecasting {
 		        char x = 'X';
 		        long y = x;
 		        System.out.println("Implicit Type Casting: int to long");
-		        System.out.println("x = " + x); // 100
-		        System.out.println("y = " + y); // 100L
+		        System.out.println("x = " + x); 
+		        System.out.println("y = " + y); 
 		        
 		        // Explicit Type Casting
 		        double a = 7.8;
 		        int b = (int) a;
 		        System.out.println("Explicit Type Casting: double to int");
-		        System.out.println("a = " + a); // 3.14
-		        System.out.println("b = " + b); // 3
+		        System.out.println("a = " + a); // 7.8
+		        System.out.println("b = " + b); // 7
 		    }
 		}
